eeef87f107d773287eca307db01eb3eb9bb49cf5
jose clavo tafur <jclavotafur@gmail.com>
2024-12-24 13:10:07 -0300

    rename template

--- src/test/scala/br/unb/cic/android/specs/_ExampleSpec.scala
+++ src/test/scala/br/unb/cic/android/specs/_ExampleSpec.scala
@@ -2,7 +2,7 @@ package br.unb.cic.android.specs
 
 import br.unb.cic.android.AndroidTaintBenchTest
 
-trait ExampleSpec extends AndroidTaintBenchTest {
+trait _ExampleSpec extends AndroidTaintBenchTest {
 //  URL https://github.com/TaintBench/roidsec/blob/master/README.md
   override val sourceList = List(
     "<android.telephony.SmsManager: android.telephony.SmsManager getDefault()>",
