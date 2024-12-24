6c7652a505b31c3a5887c7dc9f56a44bfb8fa2dd
jose clavo tafur <jclavotafur@gmail.com>
2024-12-24 00:07:48 -0300

    use standar trait

--- src/test/scala/br/unb/cic/android/AndroidTaintBenchTest.scala
+++ src/test/scala/br/unb/cic/android/AndroidTaintBenchTest.scala
@@ -20,7 +20,7 @@ import java.nio.file.Paths
 import br.unb.cic.soot.svfa.configuration.AndroidSootConfiguration
 
 class AndroidTaintBenchTest(apk: String) extends JSVFA
-    with RoidSecSpec
+    with FlowdroidSpec
     with AndroidSootConfiguration
     with Interprocedural
     with FieldSensitive
