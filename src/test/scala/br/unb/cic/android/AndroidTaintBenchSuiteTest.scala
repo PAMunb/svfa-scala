f6c12844308e996b554073289617fcbbcf2e85b0
jose clavo tafur <jclavotafur@gmail.com>
2024-12-24 11:23:10 -0300

    beita_com_beita_contact

--- src/test/scala/br/unb/cic/android/AndroidTaintBenchSuiteTest.scala
+++ src/test/scala/br/unb/cic/android/AndroidTaintBenchSuiteTest.scala
@@ -1,6 +1,6 @@
 package br.unb.cic.android
 
-import br.unb.cic.android.specs.{BackFlashSpec, RoidSecSpec}
+import br.unb.cic.android.specs.{BackFlashSpec, BeitaComBeitaContactSpec, RoidSecSpec}
 import org.scalatest.FunSuite
 
 class AndroidTaintBenchSuiteTest extends FunSuite {
@@ -14,7 +14,21 @@ class AndroidTaintBenchSuiteTest extends FunSuite {
   test("in the APK backflash, we should detect 6 flow") {
    val svfa = new AndroidTaintBenchTest("backflash") with BackFlashSpec
     svfa.buildSparseValueFlowGraph()
-    // println(svfa.svgToDotModel())
     assert(svfa.reportConflictsSVG().size == 6)
+  }
+
+  test("in the APK beita_com_beita_contact, we should detect 3 flow") {
+    val svfa = new AndroidTaintBenchTest("beita_com_beita_contact") with BeitaComBeitaContactSpec
+    svfa.buildSparseValueFlowGraph()
+    assert(svfa.reportConflictsSVG().size == 3)
+  }
+
+
+
+//template
+  test("in the XXX, we should detect X flow") {
+    val svfa = new AndroidTaintBenchTest("XXX") with BackFlashSpec
+    svfa.buildSparseValueFlowGraph()
+    assert(svfa.reportConflictsSVG().size == 0)
   }
 }
