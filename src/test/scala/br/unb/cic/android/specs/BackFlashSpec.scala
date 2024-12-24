449850d95e972f32e1340223d59a84484760facd
jose clavo tafur <jclavotafur@gmail.com>
2024-12-24 00:18:40 -0300

    sourceList

---
+++ src/test/scala/br/unb/cic/android/specs/BackFlashSpec.scala
@@ -0,0 +1,66 @@
+package br.unb.cic.android.specs
+
+import br.unb.cic.android.AndroidTaintBenchTest
+
+trait BackFlashSpec extends AndroidTaintBenchTest {
+
+  override val sourceList = List(
+    "<android.content.Intent: java.lang.String getStringExtra(java.lang.String)>",
+    "<android.content.ContentResolver: android.database.Cursor query(android.net.Uri,java.lang.String[],java.lang.String,java.lang.String[],java.lang.String)>",
+    "<android.net.Uri: android.net.Uri parse(java.lang.String)>",
+    "<java.lang.Class: java.lang.reflect.Field getDeclaredField(java.lang.String)>",
+    "<android.app.ActivityManager: java.util.List getRunningTasks(int)>",
+    "<android.content.ComponentName: java.lang.String getClassName()>",
+    "<android.telephony.TelephonyManager: java.lang.String getDeviceId()>",
+    "<java.lang.reflect.Field: java.lang.Object get(java.lang.Object)>",
+    "<android.telephony.SmsManager: java.util.ArrayList divideMessage(java.lang.String)>",
+    "<java.io.FileInputStream: void <init>",
+    "<android.webkit.WebView: void loadUrl(java.lang.String)>",
+    "<android.os.AsyncTask: java.lang.Object get()>",
+    "<android.database.Cursor: java.lang.String getString(int)>",
+    "<java.util.Calendar: java.util.Calendar getInstance()>",
+    "<java.lang.String: byte[] getBytes()>",
+    "<android.webkit.WebSettings: void setLoadWithOverviewMode(boolean)>",
+    "<android.net.ConnectivityManager: android.net.NetworkInfo getActiveNetworkInfo()>",
+    "<java.io.File: java.lang.String getPath()>",
+    "<android.webkit.WebView: void loadData(java.lang.String,java.lang.String,java.lang.String)>",
+    "<android.telephony.TelephonyManager: java.lang.String getLine1Number()>",
+    "<android.content.Intent: android.os.Bundle getExtras()>",
+    "<android.telephony.TelephonyManager: java.lang.String getNetworkOperatorName()>",
+    "<android.content.Intent: android.net.Uri getData()>",
+    "<android.telephony.SmsManager: android.telephony.SmsManager getDefault()>",
+    "<java.io.BufferedReader: java.lang.String readLine()>",
+    "<java.lang.Class: java.lang.reflect.Method getDeclaredMethod(java.lang.String,java.lang.Class[])>",
+    "<android.content.Context: java.io.FileInputStream openFileInput(java.lang.String)>",
+    "<java.lang.Class: java.lang.String getName()>",
+    "<android.telephony.SmsMessage: java.lang.String getOriginatingAddress()>",
+    "<android.provider.Settings$Secure: java.lang.String getString(android.content.ContentResolver,java.lang.String)>",
+    "<android.os.Environment: java.io.File getExternalStorageDirectory()>",
+    "<android.content.Intent: java.lang.String getAction()>",
+    "<java.util.Calendar: java.util.Date getTime()>",
+    "<android.telephony.SmsMessage: java.lang.String getMessageBody()>",
+    "<org.apache.http.HttpEntity: java.io.InputStream getContent()>",
+    "<android.database.Cursor: long getLong(int)>",
+    "<android.telephony.SmsMessage: android.telephony.SmsMessage createFromPdu(byte[])>",
+    "<java.lang.reflect.Method: java.lang.Object invoke(java.lang.Object,java.lang.Object[])>",
+    "<android.app.ActivityManager: android.os.Debug$MemoryInfo[] getProcessMemoryInfo(int[])>",
+    "<android.telephony.TelephonyManager: java.lang.String getSimCountryIso()>",
+    "<android.content.Context: java.lang.Object getSystemService(java.lang.String)>",
+    "<android.app.ActivityManager: java.util.List getRunningAppProcesses()>",
+    "<java.io.File: java.io.File[] listFiles()>",
+    "<android.os.Environment: java.lang.String getExternalStorageState()>",
+    "<java.io.DataInputStream: java.lang.String readLine()>",
+    "<java.io.File: void <init>",
+    "<java.io.File: java.lang.String getAbsolutePath()>",
+    "<java.net.HttpURLConnection: java.io.InputStream getInputStream()>",
+    "<android.telephony.SmsMessage: java.lang.String getDisplayMessageBody()>",
+    "<android.os.Bundle: java.lang.Object get(java.lang.String)>",
+    "<android.os.PowerManager: android.os.PowerManager$WakeLock newWakeLock(int,java.lang.String)>",
+    "<java.lang.Integer: int parseInt(java.lang.String)>",
+    "<android.webkit.WebView: android.webkit.WebSettings getSettings()>",
+  )
+
+  override val sinkList = List(
+    ""<java.io.File: boolean delete()>",
+  )
+}
