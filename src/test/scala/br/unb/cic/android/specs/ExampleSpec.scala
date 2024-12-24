2b69db29d1dc6ae333d099ccb46842aa38c2bdd5
jose clavo tafur <jclavotafur@gmail.com>
2024-12-24 00:12:54 -0300

    ExampleSpec

---
+++ src/test/scala/br/unb/cic/android/specs/ExampleSpec.scala
@@ -0,0 +1,14 @@
+package br.unb.cic.android.specs
+
+import br.unb.cic.android.AndroidTaintBenchTest
+
+trait ExampleSpec extends AndroidTaintBenchTest {
+//  URL https://github.com/TaintBench/roidsec/blob/master/README.md
+  override val sourceList = List(
+    "<android.telephony.SmsManager: android.telephony.SmsManager getDefault()>",
+  )
+
+  override val sinkList = List(
+    "<java.io.File: boolean delete()>",
+  )
+}
