26f3baebfe0aa34d89808b9c25944637c9c4843b
jose clavo tafur <jclavotafur@gmail.com>
2024-12-24 00:09:28 -0300

    test RoidSecSpec

--- src/test/scala/br/unb/cic/android/AndroidTaintBenchSuiteTest.scala
+++ src/test/scala/br/unb/cic/android/AndroidTaintBenchSuiteTest.scala
@@ -4,9 +4,16 @@ import org.scalatest.FunSuite
 
 class AndroidTaintBenchSuiteTest extends FunSuite {
   test("in the APK Roidsec, we should detect 1 flow") {
-    val svfa = new AndroidTaintBenchTest("roidsec")
+    val svfa = new AndroidTaintBenchTest("roidsec") with RoidSecSpec
     svfa.buildSparseValueFlowGraph()
     // println(svfa.svgToDotModel())
     assert(svfa.reportConflictsSVG().size == 1)
   }
+
+//  test("in the APK backflash, we should detect X flow") {
+//    val svfa = new AndroidTaintBenchTest("backflash")
+//    svfa.buildSparseValueFlowGraph()
+//    // println(svfa.svgToDotModel())
+//    assert(svfa.reportConflictsSVG().size == 1)
+//  }
 }
