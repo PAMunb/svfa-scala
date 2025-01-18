47e4bf0f279123335a9cf855e9cd334e06c7ac9a
jose clavo tafur <jclavotafur@gmail.com>
2025-01-18 19:27:29 -0300

    slocker_android_samp

--- src/test/scala/br/unb/cic/android/AndroidTaintBenchSuiteTest2.scala
+++ src/test/scala/br/unb/cic/android/AndroidTaintBenchSuiteTest2.scala
@@ -331,9 +331,15 @@ class AndroidTaintBenchSuiteTest2 extends FunSuite with Metrics {
   }
 
   test("in the APK slocker_android_samp, we should detect 5 flows") {
-    val svfa = new AndroidTaintBenchTest("slocker_android_samp") with SlockerAndroidSampSpec
+    val expected = 5
+
+    val svfa = new AndroidTaintBenchTest("slocker_android_samp")
     svfa.buildSparseValueFlowGraph()
-    assert(svfa.reportConflictsSVG().size == 5)
+
+    val actual = svfa.reportConflictsSVG().size
+    this.compute(expected, actual)
+
+    assert(actual == expected)
   }
 
   test("in the APK sms_google, we should detect 4 flows") {
